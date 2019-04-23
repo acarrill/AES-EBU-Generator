9d5457df4b5b8537f4ccc12bfce73898066b1ed7
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-04-23 02:26:21 +0200

    library finded, let knows bits per sample and iterate sample by sample

--- ebu.py
+++ ebu.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2
 import numpy as np
-import soundfile as sf
+import soundfile as sf # MAYBE IT WONT BE USED ////// REMEMBER DELETE IF NOT
+import wave as wv
 
 def CreateTrame():
     """
     Crea una subtrama por defecto
     """
-
+    
     Subtrame = range(32)
 
 if __name__ == "__main__":
     SubTrame = CreateTrame()
 
-    data, samplerate = sf.read('cartoon001.wav')
-    print(data.shape) 
+    audio = wv.open('cartoon001.wav', 'r')
+
+    print(audio.getsampwidth())
+    print(audio.readframes(1))
+    print(audio.tell())
