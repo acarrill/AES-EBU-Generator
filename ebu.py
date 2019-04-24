b7df7aee2d51d4db10730ec6691e4ad74884744e
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-04-24 18:55:23 +0200

    Starting to structure frames. 24 bits subframes

--- ebu.py
+++ ebu.py
@@ -3,17 +3,31 @@ import numpy as np
 import soundfile as sf # MAYBE IT WONT BE USED ////// REMEMBER DELETE IF NOT
 import wave as wv
 
-def CreateTrame():
+def CreateFrame(audio):
     """
     Crea una subtrama por defecto
     """
-    
-    Subtrame = range(32)
+
+    AudioWidht = audio.getsampwidth()  # In bytes
+    Frame = []
+    FrameList = []
+    BlockList = []
+
+    if AudioWidht == 2: #CAMBIAIAUSDJDHJASDHBASJDHBAJSHB
+        SubFrameZ = [0x02, 'MuestrasAudio', 1, 0, 0, 1]  # Trama 20 bits audio
+        SubFrameY = [0x04, 'MuestrasAudio', 1, 0, 0, 1]  # Trama 20 bits audio
+        Frame = [SubFrameZ, SubFrameY]
+
+        return FramesBlock
+
 
 if __name__ == "__main__":
-    SubTrame = CreateTrame()
+
 
     audio = wv.open('cartoon001.wav', 'r')
+
+    FramesBlock = CreateFrame(audio)
+    print(FramesBlock)
 
     print(audio.getsampwidth())
     print(audio.readframes(1))
