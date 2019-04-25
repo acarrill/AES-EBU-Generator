4a76dfeb88cc0d91b7be69da8c5235aadc5aa9c9
acarrill <a.jimenezcarr@alumnos.urjc.es>
2019-04-25 23:52:37 +0200

    Add: sys.argv for audio file;;def CreateBlock;;; def CreateFrame

--- ebu.py
+++ ebu.py
@@ -1,34 +1,59 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import numpy as np
 import soundfile as sf # MAYBE IT WONT BE USED ////// REMEMBER DELETE IF NOT
 import wave as wv
+import sys
 
-def CreateFrame(audio):
+def CreateFrame (AudioSampleX, AudioSampleY):
+    SubFrameX = [b'0xD2', 'MuestrasAudio', 1, 0, 0, 1]
+    SubFrameY = [b'0xD4', 'MuestrasAudio', 1, 0, 0, 1]
+    Frame = [SubFrameX, SubFrameY]
+    return Frame
+
+def CreateBlock(audio):
     """
     Crea una subtrama por defecto
     """
 
     AudioWidht = audio.getsampwidth()  # In bytes
-    Frame = []
     FrameList = []
     BlockList = []
 
     if AudioWidht == 2: #CAMBIAIAUSDJDHJASDHBASJDHBAJSHB
-        SubFrameZ = [0x02, 'MuestrasAudio', 1, 0, 0, 1]  # Trama 20 bits audio
-        SubFrameY = [0x04, 'MuestrasAudio', 1, 0, 0, 1]  # Trama 20 bits audio
+        # Take 2 samples and create the frist Frame
+        AudioSamples = audio.readframes(2)
+        SubFrameZ = [b'0xD8', AudioSamples[0:2], 1, 0, 0, 1]
+        SubFrameY = [b'0xD4', AudioSamples[2:5], 1, 0, 0, 1]
         Frame = [SubFrameZ, SubFrameY]
+        FrameList.append(Frame)  # First Frame with Z preamble
 
-        return FramesBlock
+        # while
+
+        return FrameList
 
 
 if __name__ == "__main__":
 
+    try:
+        AudioData = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: python ebu.py AudioFile")
 
-    audio = wv.open('cartoon001.wav', 'r')
+    with wv.open(AudioData, 'r') as audio:
 
-    FramesBlock = CreateFrame(audio)
-    print(FramesBlock)
+        FrameList = CreateBlock(audio)
 
-    print(audio.getsampwidth())
-    print(audio.readframes(1))
-    print(audio.tell())
+        print(FrameList)
+
+
+
+
+
+
+        # print(type(b'0x02'))
+        # #
+        # # print(audio.getsampwidth())
+        # # print(audio.getnframes())
+        # # sample = audio.readframes(1)
+        # # print(sample)
+        # # print(audio.tell())
